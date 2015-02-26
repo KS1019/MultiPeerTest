16cb41fb480673c53ba8cd5de734d0df28026397
KOTARO SUTO <ng1a5ss43@gmail.com>
2015-02-26 15:19:47 +0900

    Initial Commit

---
+++ MultiPeerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiPeerTest
+//
+//  Created by Kotaro Suto on 2015/02/26.
+//  Copyright (c) 2015年 Kotaro Suto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
